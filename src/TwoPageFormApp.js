c0ad8539323d253f916be2e9f2d2ab9a7a3cdab7
madilyn13glover <madilyn.glover@anheuser-busch.com>
2025-03-21 21:12:09 +0000

    fixing QD

--- src/TwoPageFormApp.js
+++ src/TwoPageFormApp.js
@@ -399,7 +399,7 @@ export default function TwoPageFormApp() {
               <th style={headerCell}>Max</th>
               <th style={headerCell}>Discount</th>
               <th style={headerCell}>Allowance</th>
-              <th style={headerCell}>AB Split</th>
+              <th style={headerCell}>AB Slit</th>
               <th style={headerCell}>Action</th>
             </tr>
           </thead>
